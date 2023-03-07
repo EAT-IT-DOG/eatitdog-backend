15d319ddfd0419874e2c0c8dd35b16350e07462d
whitebear05 <woojs0505@naver.com>
2023-03-07 18:02:26 +0900

    :wrench: :: API Docs Token 처리 위해 SwaggerConfig, WebMvcConfig 수정

--- src/main/java/com/eatitdog/eatitdog/global/config/swagger/SwaggerConfig.java
+++ src/main/java/com/eatitdog/eatitdog/global/config/swagger/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.eatitdog.eatitdog.global.config.swagger;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -8,9 +9,17 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
 @EnableWebMvc
 public class SwaggerConfig implements WebMvcConfigurer {
 
@@ -26,15 +35,37 @@ public class SwaggerConfig implements WebMvcConfigurer {
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .apiInfo(apiInfo())
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("먹어보시개")
                 .description("먹어보시개 V2 API 문서입니다.")
-                .version("V2")
+                .version("2.0.0")
                 .build();
     }
 
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
+    }
 }
--- src/main/java/com/eatitdog/eatitdog/global/config/webmvc/WebMvcConfig.java
+++ src/main/java/com/eatitdog/eatitdog/global/config/webmvc/WebMvcConfig.java
@@ -1,11 +1,17 @@
 package com.eatitdog.eatitdog.global.config.webmvc;
 
+import com.eatitdog.eatitdog.global.interceptor.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
 
     private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
             "classpath:/static/",
@@ -19,6 +25,13 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/swagger-ui/**")
+                .excludePathPatterns("/static/**");
     }
 
 }
