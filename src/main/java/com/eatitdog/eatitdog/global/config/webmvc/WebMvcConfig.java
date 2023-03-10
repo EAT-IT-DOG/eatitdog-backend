88e7bbbc7cc4201a07758ca51654fac44c1eb4d1
whitebear05 <woojs0505@naver.com>
2023-03-10 09:06:55 +0900

    :ambulance: :: WebMvcConfig addCorsMappings 적용하여 CORS 정책 변경

--- src/main/java/com/eatitdog/eatitdog/global/config/webmvc/WebMvcConfig.java
+++ src/main/java/com/eatitdog/eatitdog/global/config/webmvc/WebMvcConfig.java
@@ -2,7 +2,9 @@ package com.eatitdog.eatitdog.global.config.webmvc;
 
 import com.eatitdog.eatitdog.global.interceptor.AuthInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -27,11 +29,21 @@ public class WebMvcConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
     }
 
+    @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(authInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/swagger-ui/**")
                 .excludePathPatterns("/static/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .allowCredentials(false);
     }
 
 }
