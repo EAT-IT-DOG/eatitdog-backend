7f28f19f73c887125c2bb9841fa1affb35ccd88a
whitebear05 <woojs0505@naver.com>
2023-05-09 10:48:41 +0900

    :fire: :: apply 기능 드랍 (사용성 부족)

--- src/main/java/com/eatitdog/eatitdog/domain/apply/domain/repository/ApplyRepository.java
+++
@@ -1,7 +0,0 @@
-package com.eatitdog.eatitdog.domain.apply.domain.repository;
-
-import com.eatitdog.eatitdog.domain.apply.domain.Apply;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ApplyRepository extends JpaRepository<Apply, Long> {
-}
